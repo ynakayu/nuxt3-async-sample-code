0c374028689456a883aa281e28e6d9931630c129
ynakayu <9111754+ynakayu@users.noreply.github.com>
2023-04-03 11:40:58 +0900

    06-04-show-a-item

--- app.vue
+++ app.vue
@@ -1,5 +1,8 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <p>{{ users[0].id }}, {{ users[0].name }} </p>
   </div>
 </template>
+<script setup>
+  const { data: users } = await useFetch('https://jsonplaceholder.typicode.com/users')
+</script>
